692d3c596e2810d7b7bd182da17706fd53b6d18d
KarthikDevadiga <kdevadiga497@gmail.com>
2025-01-30 19:26:17 +0530

    Ready for production

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { lazy, Suspense } from "react";
 import Form from "./coponents/Form";
-import AppLayout from "./pages/AppLayout";
-
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Login from "./pages/Login";
-
+import SpinnerFullPage from "./coponents/SpinnerFullPage";
 import ProtectedRoute from "./coponents/ProtectedRoute";
 
 import CityList from "./coponents/cityList";
@@ -16,38 +11,46 @@ import { CityContext } from "./contexts/CityContex";
 
 import { AuthProvider } from "./contexts/AuthContext";
 
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+
 function App() {
   return (
     <>
       <CityContext>
         <AuthProvider>
           <BrowserRouter future={{ v7_relativeSplatPath: true }}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route
-                path="app"
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route index element={<Navigate replace to={"cities"} />} />
-                <Route path="cities" element={<CityList c />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="countries" element={<CountryList />} />
-                <Route path="form" element={<Form />} />
-              </Route>
-              <Route path="pricing" element={<Pricing />} />
-              <Route path="product" element={<Product />} />
+            <Suspense fallback={<SpinnerFullPage />}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route
+                  path="app"
+                  element={
+                    <ProtectedRoute>
+                      <AppLayout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Navigate replace to={"cities"} />} />
+                  <Route path="cities" element={<CityList c />} />
+                  <Route path="cities/:id" element={<City />} />
+                  <Route path="countries" element={<CountryList />} />
+                  <Route path="form" element={<Form />} />
+                </Route>
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="product" element={<Product />} />
 
-              <Route path="login" element={<Login />} />
+                <Route path="login" element={<Login />} />
 
-              <Route
-                path="*"
-                element={<p style={{ color: "black" }}>404 not found :</p>}
-              />
-            </Routes>
+                <Route
+                  path="*"
+                  element={<p style={{ color: "black" }}>404 not found :</p>}
+                />
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </AuthProvider>
       </CityContext>
